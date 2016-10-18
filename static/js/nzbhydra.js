fba1ca87b6d21845db034827cb6ceb314d3093b1
Dave Hunsberger <petrichor8@users.noreply.github.com>
2016-10-18 08:57:35 -0400

    Update nzbhydra.js
    change url from absolute to relative to enable backups page to be called.

--- static/js/nzbhydra.js
+++ static/js/nzbhydra.js
@@ -1321,7 +1321,7 @@ function hydrabackup() {
 
         $scope.createAndDownloadBackupFile = function() {
 
-                $http({method: 'GET', url: '/internalapi/getbackup', responseType: 'arraybuffer'}).success(function (data, status, headers, config) {
+                $http({method: 'GET', url: 'internalapi/getbackup', responseType: 'arraybuffer'}).success(function (data, status, headers, config) {
                     var a = document.createElement('a');
                     var blob = new Blob([data], {'type': "application/octet-stream"});
                     a.href = URL.createObjectURL(blob);
